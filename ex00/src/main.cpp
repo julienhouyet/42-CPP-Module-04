152dc4708414321d3e63a93518ef150ccb16b33d
Julien Houyet <julien.houyet@gmail.com>
2024-06-09 12:04:22 +0200

    refactor: add tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 07:29:19 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/09 12:01:01 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/09 12:03:40 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,26 @@
 
 int	main()
 {
+	/*
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	delete i;
+	delete j;
+	delete meta;
+	*/
+
+	const WrongAnimal* meta = new WrongAnimal();
+	const Animal* j = new Dog();
+	const WrongAnimal* i = new WrongCat();
 
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
